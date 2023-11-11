9c3353d614910d13a0a78632db593134dd3bb04c
Guy Avraham <76410482+guyavrhm@users.noreply.github.com>
2023-11-11 04:01:16 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
 import cv2
 import numpy as np
 
-def track_blue_object(video_source=0):
+
+def bgr_to_hsv_limits(bgr_color, tolerance = 10):
+    hsv_color = cv2.cvtColor(np.uint8([[bgr_color]]), cv2.COLOR_BGR2HSV)
+    print(hsv_color)
+
+    lower_limit = np.array([max(0, hsv_color[0][0][0] - tolerance), 100, 100], dtype=np.uint8)
+    upper_limit = np.array([min(179, hsv_color[0][0][0] + tolerance), 255, 255], dtype=np.uint8)
+
+    return lower_limit, upper_limit
+
+
+def track_object(lower_color, upper_color, video_source=0):
     cap = cv2.VideoCapture(video_source)
 
-    cv2.namedWindow("Blue Object Trackingq", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Object Tracking", cv2.WINDOW_NORMAL)
     cv2.namedWindow("HSV Frame", cv2.WINDOW_NORMAL)
     cv2.namedWindow("Mask Frame", cv2.WINDOW_NORMAL)
 
@@ -15,10 +26,7 @@ def track_blue_object(video_source=0):
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        lower_blue = np.array([100, 50, 50])
-        upper_blue = np.array([130, 255, 255])
-
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask = cv2.inRange(hsv, lower_color, upper_color)
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if contours:
@@ -30,10 +38,10 @@ def track_blue_object(video_source=0):
                 cy = int(M["m01"] / M["m00"])
 
                 # Draw a circle at the centroid
-                cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+                cv2.circle(frame, (cx, cy), 3, (0, 255, 0), -1)
             print(cx, cy);
 
-        cv2.imshow("Blue Object Tracking", frame)
+        cv2.imshow("Object Tracking", frame)
         cv2.imshow("HSV Frame", hsv)
         cv2.imshow("Mask Frame", mask)
 
@@ -43,5 +51,10 @@ def track_blue_object(video_source=0):
     cap.release()
     cv2.destroyAllWindows()
 
-track_blue_object()
+bgr_color = [255, 0, 0]
+print(bgr_color)
+lower, upper = bgr_to_hsv_limits(bgr_color, tolerance=30)
+print("Lower Limit:", lower)
+print("Upper Limit:", upper)
+track_object(lower, upper)
 
