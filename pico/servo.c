eab846410a2c87a12e368f4f864329aee9dcc276
Guy Avraham <guy.avraham03@gmail.com>
2023-11-23 17:13:16 +0200

    fix servo pwm

--- pico/servo.c
+++ pico/servo.c
@@ -1,14 +1,20 @@
 #include "servo.h"
 
-#define CLKDIV clock_get_hz(clk_sys)/(SERVO_FREQ*WRAP)
-
 void servo_init(uint servo_pin) {
   gpio_set_function(servo_pin, GPIO_FUNC_PWM);
   uint slice_num = pwm_gpio_to_slice_num(servo_pin);
 
   pwm_config config = pwm_get_default_config();
-  pwm_config_set_clkdiv(&config, CLKDIV);
-  pwm_config_set_wrap(&config, WRAP);
+
+  // Clock divider to achieve low-frequency PWM
+  // System clock / Desired frequency = clkdiv
+  uint32_t clkdiv = clock_get_hz(clk_sys) / PWM_FREQUENCY;
+  pwm_config_set_clkdiv(&config, clkdiv);
+
+  // Wrap for servo motor frequency:
+  // Servo frequency cycle / New frequency cycle or
+  // New frequency / Servo frequency
+  pwm_config_set_wrap(&config, PWM_FREQUENCY / SERVO_FREQUENCY);
 
   pwm_init(slice_num, &config, true);
 }
@@ -20,8 +26,9 @@ void servo_set_angle(uint servo_pin, float degs) {
     degs = 180.0f;
   }
 
-  // map angle (0-180) to ms (500-2500)
+  // Map angle (0-180) to pulse (500us-2500us)
   float pulse_width = 500.0f + (degs / 180.0f) * 2000.0f;
-
-  pwm_set_gpio_level(servo_pin, (pulse_width / ((1 / SERVO_FREQ) * 1e6))*WRAP);
+  
+  // Level (Duty Cycle) (0-WRAP) = WRAP / ( cycle (s) / pulse (s) )
+  pwm_set_gpio_level(servo_pin, (PWM_FREQUENCY / SERVO_FREQUENCY) / ((1.0f / SERVO_FREQUENCY) / (pulse_width / 1e6)));
 }
--- pico/servo.h
+++ pico/servo.h
@@ -1,12 +1,13 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/clocks.h"
 
-#define WRAP 10000.f
-#define SERVO_FREQ 50.f
+#define PWM_FREQUENCY   500000.0f
+#define SERVO_FREQUENCY 50.0f
 
 void servo_init(uint servo_pin);
 void servo_set_angle(uint servo_pin, float degrees);
